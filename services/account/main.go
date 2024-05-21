32103373d8a549b4afca0805fed374ec56b240c1
dddddjent <1306043330a@gmail.com>
2024-05-21 22:03:55 +0800

    Docker, Nginx

--- services/account/main.go
+++ services/account/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Account service begins.")
+
+	r := gin.Default()
+
+	r.GET("/api/account", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "O",
+		})
+	})
+
+	r.Run(":8080")
 }
