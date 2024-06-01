ff9126cf003e87639d8b677aac1e6fe740755888
dddddjent <1306043330a@gmail.com>
2024-06-01 23:48:08 +0800

    No account to sign in -> goto sign up

--- client/src/app/signin.tsx
+++ client/src/app/signin.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom'
 
 type SignInResponse = {
     token_pair: {
-        access_token: string,
+        access_token: string
         refresh_token: {
             token_string: string
         }
@@ -25,23 +25,25 @@ const SignIn = function() {
 
     const submit = async () => {
         if (password.length == 0) {
-            setErrorMessage("Password is missing")
+            setErrorMessage('Password is missing')
             return
         }
         if (password.length < 6 || password.length > 10) {
-            setErrorMessage("Password should be greater or equal to 6 and less than or equal to 10 characters long.")
+            setErrorMessage(
+                'Password should be greater or equal to 6 and less than or equal to 10 characters long.',
+            )
             return
         }
         if (userName.length === 0 && !useEmail) {
-            setErrorMessage("User name is missing.")
+            setErrorMessage('User name is missing.')
             return
         }
         if (userName.length > 30) {
-            setErrorMessage("User name should be less or equal to 30 characters.")
+            setErrorMessage('User name should be less or equal to 30 characters.')
             return
         }
         if (email.length === 0 && useEmail) {
-            setErrorMessage("Email is missing.")
+            setErrorMessage('Email is missing.')
             return
         }
 
@@ -54,26 +56,29 @@ const SignIn = function() {
                     password: password,
                 },
             )
-            localStorage.setItem("access_token", data.token_pair.access_token)
-            localStorage.setItem("refresh_token", data.token_pair.refresh_token.token_string)
-            setErrorMessage("")
-            return navigate("/dashboard")
+            localStorage.setItem('access_token', data.token_pair.access_token)
+            localStorage.setItem(
+                'refresh_token',
+                data.token_pair.refresh_token.token_string,
+            )
+            setErrorMessage('')
+            return navigate('/dashboard')
         } catch (error) {
             asAxiosError(error, (error) => {
                 console.log(error.response)
                 switch (error.response?.status) {
                     case 400: {
-                        setErrorMessage("Bad request.")
-                        break;
+                        setErrorMessage('Bad request.')
+                        break
                     }
                     case 401: {
-                        const id = useEmail ? "Email" : "User name"
+                        const id = useEmail ? 'Email' : 'User name'
                         setErrorMessage(`${id} or password is not correct.`)
-                        break;
+                        break
                     }
                     case 500: {
-                        setErrorMessage("Server is not responding. Please wait.")
-                        break;
+                        setErrorMessage('Server is not responding. Please wait.')
+                        break
                     }
                 }
             })
@@ -86,30 +91,34 @@ const SignIn = function() {
                 <div className='signin-title'>Sign In To Your Account</div>
 
                 <div className='signin-form'>
-                    {!useEmail && <div className='signin-row'>
-                        <label className='signin-label'>User Name</label>
-                        <input
-                            type='text'
-                            placeholder='Your user name'
-                            value={userName}
-                            onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                setUserName(e.target.value)
-                            }}
-                            className='signin-input'
-                        ></input>
-                    </div>}
-                    {useEmail && <div className='signin-row'>
-                        <label className='signin-label'>Email</label>
-                        <input
-                            type='text'
-                            placeholder='Your email'
-                            value={email}
-                            onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                setEmail(e.target.value)
-                            }}
-                            className='signin-input'
-                        ></input>
-                    </div>}
+                    {!useEmail && (
+                        <div className='signin-row'>
+                            <label className='signin-label'>User Name</label>
+                            <input
+                                type='text'
+                                placeholder='Your user name'
+                                value={userName}
+                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setUserName(e.target.value)
+                                }}
+                                className='signin-input'
+                            ></input>
+                        </div>
+                    )}
+                    {useEmail && (
+                        <div className='signin-row'>
+                            <label className='signin-label'>Email</label>
+                            <input
+                                type='text'
+                                placeholder='Your email'
+                                value={email}
+                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setEmail(e.target.value)
+                                }}
+                                className='signin-input'
+                            ></input>
+                        </div>
+                    )}
                     <div className='signin-row'>
                         <label className='signin-label'>Password</label>
                         <input
@@ -124,18 +133,32 @@ const SignIn = function() {
                     </div>
                 </div>
                 <div className='signin-switch-signin-method'>
-                    <button className='signin-switch-signin-button' onClick={() => {
-                        if (useEmail) {
-                            setEmail('')
-                        }
-                        else {
-                            setUserName('')
-                        }
-                        setUseEmail(!useEmail)
-                    }}>{useEmail ? "Use user name" : "Use email"}</button>
+                    <button
+                        className='signin-switch-signin-button'
+                        onClick={() => {
+                            if (useEmail) {
+                                setEmail('')
+                            } else {
+                                setUserName('')
+                            }
+                            setUseEmail(!useEmail)
+                        }}
+                    >
+                        {useEmail ? 'Use user name' : 'Use email'}
+                    </button>
                 </div>
                 <div className='signin-error-message'>{errorMessage}</div>
-                <button className='signin-submit' onClick={submit}>Sign In</button>
+                <button className='signin-submit' onClick={submit}>
+                    Sign In
+                </button>
+                <div className='signin-to-signup'>
+                    <button
+                        className='signin-to-signup-button'
+                        onClick={() => navigate('/signup')}
+                    >
+                        No account? Goto sign up
+                    </button>
+                </div>
             </div>
         </div>
     )
