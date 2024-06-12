fbfe397c85dfe8779f07450a7d2028b26cb516b1
dddddjent <1306043330a@gmail.com>
2024-06-12 11:35:12 +0800

    Refine page selector

--- client/src/app/dashboard/all_words/page_selector.tsx
+++ client/src/app/dashboard/all_words/page_selector.tsx
@@ -9,57 +9,44 @@ export interface PageSelectorInfo {
 }
 
 export default function PageSelector(info: PageSelectorInfo) {
+	const pageNumComponent = (page: number): JSX.Element => {
+		return (
+			<span
+				key={uuidv4()}
+				className='page-number'
+				onClick={() => {
+					info.onPageChange(page)
+				}}
+				style={
+					info.page == page
+						? {
+								color: 'red',
+								// eslint-disable-next-line no-mixed-spaces-and-tabs
+						  }
+						: {}
+				}
+			>
+				{page}
+			</span>
+		)
+	}
 	const pageList = (() => {
 		const list: Array<JSX.Element> = []
 		if (info.pageCnt <= 5) {
 			for (let i = 1; i <= info.pageCnt; i++) {
-				list.push(
-					<span
-						key={uuidv4()}
-						className='page-number'
-						onClick={() => {
-							info.onPageChange(i)
-						}}
-						style={
-							info.page == i
-								? {
-										color: 'red',
-										// eslint-disable-next-line no-mixed-spaces-and-tabs
-								  }
-								: {}
-						}
-					>
-						{i}
-					</span>,
-				)
+				list.push(pageNumComponent(i))
 			}
 		} else {
 			for (let i = 1; i <= 3; i++) {
-				list.push(
-					<span
-						key={uuidv4()}
-						className='page-number'
-						onClick={() => {
-							info.onPageChange(i)
-						}}
-					>
-						{i}
-					</span>,
-				)
+				list.push(pageNumComponent(i))
 			}
 			list.push(<span key={uuidv4()}>...</span>)
+			if (info.page > 3 && info.page < info.pageCnt - 1) {
+				list.push(pageNumComponent(info.page))
+				list.push(<span key={uuidv4()}>...</span>)
+			}
 			for (let i = info.pageCnt - 1; i <= info.pageCnt; i++) {
-				list.push(
-					<span
-						key={uuidv4()}
-						className='page-number'
-						onClick={() => {
-							info.onPageChange(i)
-						}}
-					>
-						{i}
-					</span>,
-				)
+				list.push(pageNumComponent(i))
 			}
 		}
 		return list
